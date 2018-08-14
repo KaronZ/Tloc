38f8cdac24e97d77a021e95b4a9f799824fb4d98
Karon Zhang <zyige_1214@outlook.com>
2018-08-14 16:48:03 +0800

    Update run_Example.py

--- run_Example.py
+++ run_Example.py
@@ -13,7 +13,7 @@ import transferForest as trans
 
 if __name__=='__main__':
     
-    dataSet=loadCSV('/xx/xx.csv') #your file path, the file consists of source domain data and target domian data
+    dataSet = loadCSV('/xx/xx.csv') #your file path, the file consists of source domain data and target domian data
     rf.column_to_float(dataSet)
     targetID = 'xxxx' #specifiy the target domain  ID
     
@@ -27,25 +27,34 @@ if __name__=='__main__':
     n_trees = 100
     
     #cross validation
-    folds=spiltDataSet(dataSet,n_folds)
+    folds=spiltDataSet(dataSet, n_folds)
     for fold in folds:
-        train_set=folds[:]  
-        train_set=sum(train_set,[]) 
-        test_set=[]
+        train_set = folds[:]  
+        train_set = sum(train_set,[]) 
+        test_set = []
         for row in fold:
-            row_copy=list(row)
-            row_copy[-1]=None
+            row_copy = list(row)
+            row_copy[-1] = None
             test_set.append(row_copy)
         
         actual=[row[-1] for row in fold]
         
+        
+        #====================train and test a transfer forest regression model=========================================      
         # train a transfer forest
         transfer_forest = trans.transferForest(train_set, targetID, n_features, max_depth, min_size, n_trees)
         
-        #predict test data
+        # make predictions test data
         predict_values = trans.transfer_forest_predict(transfer_forest, test_set)
         
-        #compute error
+        # compute error
+        error = rf.accuracy(predict_values, actual)
+        
+        #====================train and test a normal random froest regression model======================================
+        # train a model and make predictions on test data
+        predict_values = rf.random_forest(train_set, test_set, ratio, n_features, max_depth, min_size, n_trees)
+
+        # compute error
         error = rf.accuracy(predict_values, actual)
         
 
