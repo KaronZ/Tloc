c217143beccaf7fd3bae2daac5e7029005919c5d
Karon Zhang <zyige_1214@outlook.com>
2018-08-14 17:58:22 +0800

    Update transferForest.py

--- transferForest.py
+++ transferForest.py
@@ -1,6 +1,6 @@
+
 # coding: utf-8
 
-# In[2]:
 
 import csv
 from random import seed
@@ -14,7 +14,23 @@ import scipy.stats
 import STRUT as strut
 
 
-# In[3]:
+def weight_loss(w_m, w_r, target, feature_mtl, value_mtl, value_str):
+	left_m, right_m = rf.data_spilt(target, feature_mtl, value_mtl)
+    loss_m = rf.spilt_loss(left,right)
+	
+	left_r, right_r = rf.data_spilt(target, feature_mtl, value_str)
+    loss_r = rf.spilt_loss(left,right)
+	
+	beta = 0.8
+	
+	w_m_t = w_m * (beta ** loss_m)
+	w_r_t = w_m * (beta ** loss_r)
+	
+	weight_m = w_m_t / (w_m_t + w_r_t)
+	weight_r = w_r_t / (w_m_t + w_r_t)
+	
+	return weight_m, weight_r
+	
 
 def transferForest(train, targetID, n_features, max_depth, min_size, n_trees):
     transfer_trees =[]
@@ -32,6 +48,9 @@ def transferForest(train, targetID, n_features, max_depth, min_size, n_trees):
     feature_list = []
     value_list = []
 
+	w_m = 0.5
+	w_r = 0.5
+    
     for i in range(n_trees):
         normal_tree = rf.get_best_spilt_candidate(dataSet,n_features)
         
@@ -42,7 +61,9 @@ def transferForest(train, targetID, n_features, max_depth, min_size, n_trees):
         feature_mtl, value_mtl = mtl.multi_loss(source_list, target, feature_list, value_list)
         value_str = strut.optimize(train, target, feature_mtl, value_mtl)
         
-        spilt_value = 0.5 * value_mtl + 0.5 * value_str
+        spilt_value = w_m * value_mtl + w_r * value_str
+		w_m, w_r = weight_loss(w_m, w_r, target, feature_mtl, value_mtl, value_str)
+		
         tree = rf.build_transfer_tree(train, feature_mtl, split_value, max_depth, min_size)
         
         transfer_trees.append(tree)
@@ -50,8 +71,8 @@ def transferForest(train, targetID, n_features, max_depth, min_size, n_trees):
     return transfer_trees
 
 
-# In[4]:
 
 def transfer_forest_predict(train_model, test):
     predict_values = [rf.bagging_predict(train_model, row) for row in test]
     return predict_values
+
